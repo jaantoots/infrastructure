a3d864029ac663df5d6f1d309a7baa38c05892e9
Jaan Toots <jaan@jaantoots.org>
2019-09-24 23:33:39 +0100

    Move Linode provider configuration to main

--- linode.tf
+++ linode.tf
@@ -1,19 +1,3 @@
-provider "linode" {
-  version = "~> 1.8"
-}
-
-resource "linode_sshkey" "caracal" {
-  label   = "caracal"
-  ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPMTSn9LSly4Gic5/s9JqYfvRTi+ZkwoZd/YTWaxmPA7 jaan@caracal"
-}
-
-resource "linode_sshkey" "falstaff" {
-  label   = "falstaff"
-  ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN0xreG4XPAgqvyPqP58HYMGTjHKyh59XGDdOUgjVIEN jaan@falstaff"
-}
-
-data "linode_profile" "me" {}
-
 resource "linode_instance" "cloud" {
   label            = "arch-eu-central"
   region           = "eu-central"
--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Cloudflare for DNS
 provider "cloudflare" {
   version = "~> 1.18"
 }
@@ -6,3 +7,20 @@ variable "cloudflare_zone" {
   type    = string
   default = "jaan.xyz"
 }
+
+# Linode for servers
+provider "linode" {
+  version = "~> 1.8"
+}
+
+resource "linode_sshkey" "caracal" {
+  label   = "caracal"
+  ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPMTSn9LSly4Gic5/s9JqYfvRTi+ZkwoZd/YTWaxmPA7 jaan@caracal"
+}
+
+resource "linode_sshkey" "falstaff" {
+  label   = "falstaff"
+  ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN0xreG4XPAgqvyPqP58HYMGTjHKyh59XGDdOUgjVIEN jaan@falstaff"
+}
+
+data "linode_profile" "me" {}

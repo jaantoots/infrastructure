2270f364139f70d2f914553a297c553691abcb95
Jaan Toots <jaan@jaantoots.org>
2019-11-08 01:13:22 +0200

    Add password store and gpslogger to cloud

--- cloud.tf
+++ cloud.tf
@@ -27,14 +27,25 @@ data "ignition_config" "cloud" {
     "${data.ignition_filesystem.cloud_data.id}",
   ]
   directories = [
+    "${data.ignition_directory.authorized_keys.id}",
+    "${data.ignition_directory.data_pass.id}",
+    "${data.ignition_directory.data_gps.id}",
     "${data.ignition_directory.data_taskd.id}",
   ]
   files = [
     "${data.ignition_file.docker_auth.id}",
+    "${data.ignition_file.sshd_config.id}",
+    "${data.ignition_file.pass_auth.id}",
+    "${data.ignition_file.gps_auth.id}",
   ]
   systemd = [
     "${data.ignition_systemd_unit.data_mount.id}",
+    "${data.ignition_systemd_unit.pass.id}",
     "${data.ignition_systemd_unit.taskserver.id}",
+  ]
+  users = [
+    "${data.ignition_user.pass.id}",
+    "${data.ignition_user.gps.id}",
   ]
 }
 
@@ -44,6 +55,108 @@ data "ignition_filesystem" "cloud_data" {
     device = "/dev/disk/by-label/data"
     format = "ext4"
   }
+}
+
+data "ignition_file" "sshd_config" {
+  filesystem = "root"
+  path       = "/etc/ssh/sshd_config"
+  mode       = 420
+  content {
+    content = <<EOF
+# Use most defaults for sshd configuration.
+Subsystem sftp internal-sftp
+ClientAliveInterval 180
+UseDNS no
+UsePAM yes
+PrintLastLog no # handled by PAM
+PrintMotd no # handled by PAM
+
+# Custom configuration
+Match User ${data.ignition_user.pass.name},${data.ignition_user.gps.name}
+    AuthorizedKeysFile /etc/ssh/authorized_keys/%u
+    AllowTcpForwarding no
+EOF
+  }
+}
+
+data "ignition_directory" "authorized_keys" {
+  filesystem = "root"
+  path       = "/etc/ssh/authorized_keys"
+  mode       = 493
+}
+
+data "ignition_user" "pass" {
+  name           = "pass"
+  home_dir       = "/data/pass"
+  no_create_home = true
+  shell          = "/usr/bin/git-shell"
+  uid            = 1010
+}
+
+data "ignition_file" "pass_auth" {
+  filesystem = "root"
+  path       = "/etc/ssh/authorized_keys/${data.ignition_user.pass.name}"
+  mode       = 420
+  content {
+    content = <<EOF
+%{for _, key in var.ssh_keys~}
+${key}
+%{endfor~}
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCw/nzdTrfL01ZEeFn3Os7XCOb8prZWp+5h5m5pfpDKX8qUoI+cELF4p+u8bkzSONFBoFYAbvzRgW9QkBCRnopyLY4c7q5R+Rxy4CqwhfaLzFL1Ova8uBsEhEHhEwhwX+kWG/hDCbq6AppStw+JCNkWOcj0fjntfucGoqRbrv9kS4+y1JjgvFMQeHaKif0ZILb/SivhHGMaJpApyHWx8kmkDm0AFjb3Gx0BZBX1atQHi+l8yfcJpDMwm50M3KH1PDWL1GGleqjV+yQ5XGlh2VXV7CyUTrE/yRnchl6M+831Btq1SvrfmagZ46xRDSFEKWvsevYC72HvwfpcxsYv5yPTETGnI2/vE2lAihI44vg8u1uJ3TkPJKfu8zXRUy3VAwOaSQNc5J09RSQY2x9UMCNq7XUGTlz1p1BdzMulpJRMj7oZI0wqzqsvNGRvG+jw8lxei1k38OztTXvDbf7P7/mM77jYLC7uorWASFQ3D5TgXDnb8dpRo8h8BX65UlE6pG3bbndI5wKrqdjt/mACDdYBzPkc8dRz7MYs1HYFnv8BbzKLRNr6qMAsEj5Q9nNtKsfd2ZONE6Ce+fHrIFMMHyPTikFqyXm8/65YVW0OemP3I93GJdkvRevkb8hq380ABwGcS+EwyV5EIfEI0bUWJFw2/ZQo2Y+988AfJ9P0qeBHWw== jaan@oneplus5t
+EOF
+  }
+}
+
+data "ignition_directory" "data_pass" {
+  filesystem = "data"
+  path       = "/pass"
+  mode       = 493
+  uid        = data.ignition_user.pass.uid
+}
+
+data "ignition_systemd_unit" "pass" {
+  name    = "password-store.service"
+  content = <<EOF
+[Unit]
+Description=Initialize password store
+
+[Service]
+Type=oneshot
+User=${data.ignition_user.pass.name}
+UMask=0077
+ExecStart=/usr/bin/git init --bare ${data.ignition_user.pass.home_dir}/password-store.git
+
+[Install]
+WantedBy=multi-user.target
+EOF
+}
+
+data "ignition_user" "gps" {
+  name           = "gps"
+  home_dir       = "/data/gps"
+  no_create_home = true
+  uid            = 1011
+}
+
+data "ignition_file" "gps_auth" {
+  filesystem = "root"
+  path       = "/etc/ssh/authorized_keys/${data.ignition_user.gps.name}"
+  mode       = 420
+  content {
+    content = <<EOF
+%{for _, key in var.ssh_keys~}
+${key}
+%{endfor~}
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDHIoP5UCYh1+K432lXEk7QhiPKVa23QEtASPXLhAf54zqRy3BYWvjJRSxaJHUqxGIvc7j1SY0PbeRG9idq+XZclxjFGTsOeLqFJ/eFIt8DFD/HNrDa5pwGH7MznAbD/kYxm641D+UMiBphROfDdMFN4bQv12Bj6Tamq+be29uTUAZABanhKndmKFm2cKs9C1G7Q4EaNMYfVn9Exzy6DzulwgW+8HhPrU5imeTlFNMJIn6DTCsdc8mZ6zUF+1sp5+gCST+GahNyFGqY8cgBs1UeJ5fcgyoe8Bnl/i5OGK931pWdz2IMs1dE/Gxmj6kvJvxdiOKtVUXO+nS5ebOTP/dH gpslogger@oneplus5t
+EOF
+  }
+}
+
+data "ignition_directory" "data_gps" {
+  filesystem = "data"
+  path       = "/gps"
+  mode       = 493
+  uid        = data.ignition_user.gps.uid
 }
 
 data "ignition_directory" "data_taskd" {

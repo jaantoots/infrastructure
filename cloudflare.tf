f2efc881560d23dd209a2fbcce0e5b0df0d5ccef
Jaan Toots <jaan@jaantoots.org>
2019-12-01 15:42:30 +0200

    Add Backblaze with Cloudflare

--- cloudflare.tf
+++ cloudflare.tf
@@ -39,6 +39,37 @@ module "mailgun_jaan_xyz" {
   domainkey = "k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC9jJejlv+0qFFTwHNgJbz0cf+m2POJ1kDt6l3bGG41wFTG/fT8jp/yFWNgeXJeB0EPKGJK1H3DGobGjdfc0SS4kzXLkWTN7XIVbvl6U669Vw5EKDuP7XGdEARqata71O2jDwXUeCsZ5jIzPG/2FJtIl/90dUaEN2DtGdGgtdjuywIDAQAB"
 }
 
+resource "cloudflare_record" "cdn" {
+  zone_id = "${cloudflare_zone.jaan_xyz.id}"
+  name    = "cdn"
+  type    = "CNAME"
+  value   = "f002.backblazeb2.com"
+  proxied = true
+}
+
+resource "cloudflare_page_rule" "allow_bucket" {
+  zone_id  = "${cloudflare_zone.jaan_xyz.id}"
+  target   = "https://${cloudflare_record.cdn.hostname}/file/jaan-public/*"
+  priority = 2
+
+  actions {
+    ssl = "full"
+  }
+}
+
+resource "cloudflare_page_rule" "disallow_others" {
+  zone_id  = "${cloudflare_zone.jaan_xyz.id}"
+  target   = "https://${cloudflare_record.cdn.hostname}/file/*/*"
+  priority = 1
+
+  actions {
+    forwarding_url {
+      url         = "https://secure.backblaze.com/404notfound"
+      status_code = 302
+    }
+  }
+}
+
 resource "cloudflare_zone" "jstd_io" {
   zone = "jstd.io"
 }

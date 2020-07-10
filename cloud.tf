24051827f91963eadffce4cbdf3c3a006821bd11
Jaan Toots <jaan@jaantoots.org>
2020-07-10 13:23:53 +0300

    Fix security group rule to allow ICMP

--- cloud.tf
+++ cloud.tf
@@ -30,10 +30,10 @@ resource "aws_security_group_rule" "cloud_allow_https" {
   security_group_id = aws_security_group.cloud.id
 }
 
-resource "aws_security_group_rule" "cloud_allow_ping" {
+resource "aws_security_group_rule" "cloud_allow_icmp" {
   type              = "ingress"
-  from_port         = 0
-  to_port           = 0
+  from_port         = -1
+  to_port           = -1
   protocol          = "icmp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.cloud.id

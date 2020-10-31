bc3f2d084518d6dd1f00ec55f5d6b720beae13a2
Jaan Toots <jaan@jaantoots.org>
2020-11-01 01:25:44 +0200

    Add blog to gitlab

--- www.tf
+++ www.tf
@@ -18,3 +18,17 @@ resource "cloudflare_record" "root" {
   type    = "CNAME"
   value   = "www.${cloudflare_zone.jaan_xyz.zone}"
 }
+
+resource "cloudflare_record" "blog" {
+  zone_id = cloudflare_zone.jaan_xyz.id
+  name    = "blog"
+  type    = "CNAME"
+  value   = "jaantoots.gitlab.io"
+}
+
+resource "cloudflare_record" "blog_verify" {
+  zone_id = cloudflare_zone.jaan_xyz.id
+  name    = "_gitlab-pages-verification-code.blog"
+  type    = "TXT"
+  value   = "gitlab-pages-verification-code=839b7193500007e2920137217cea2545"
+}

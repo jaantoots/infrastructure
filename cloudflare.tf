ccd27079d0fb2e125ab50dabc5410dd5696cf527
Jaan Toots <jaan@jaantoots.org>
2020-12-17 23:55:37 +0200

    Remove mailgun

--- cloudflare.tf
+++ cloudflare.tf
@@ -28,13 +28,6 @@ module "fastmail_jaantoots_org" {
   zone   = cloudflare_zone.jaantoots_org
 }
 
-module "mailgun_jaantoots_org" {
-  source    = "./mailgun"
-  zone      = cloudflare_zone.jaantoots_org
-  selector  = "krs"
-  domainkey = "k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDhQNpN9thb0zIz9JCA+QzM2YFRnPYE+3Tv6Q33/UwBKbTDbevhkMJ7e1QBjAWr6AXOO/VpYcaTpJJSLA2xdNzTyhp8kWyVnjcsUc0ifYXoXPEziY1p4qTDFPLrusfmAkPQzSRMdlFWhN00Lm2KKCrRjZ+a60YiUYDSUmTMiohbjQIDAQAB"
-}
-
 resource "cloudflare_zone" "jaan_xyz" {
   zone = "jaan.xyz"
 }
@@ -43,13 +36,6 @@ module "fastmail_jaan_xyz" {
   source         = "./fastmail"
   zone           = cloudflare_zone.jaan_xyz
   extra_mx_names = ["www", "cloud"]
-}
-
-module "mailgun_jaan_xyz" {
-  source    = "./mailgun"
-  zone      = cloudflare_zone.jaan_xyz
-  selector  = "smtp"
-  domainkey = "k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC9jJejlv+0qFFTwHNgJbz0cf+m2POJ1kDt6l3bGG41wFTG/fT8jp/yFWNgeXJeB0EPKGJK1H3DGobGjdfc0SS4kzXLkWTN7XIVbvl6U669Vw5EKDuP7XGdEARqata71O2jDwXUeCsZ5jIzPG/2FJtIl/90dUaEN2DtGdGgtdjuywIDAQAB"
 }
 
 resource "cloudflare_record" "cdn" {
--- mailgun/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "~> 2.0"
-    }
-  }
-  required_version = ">= 0.13"
-}
